a0e094482f94327fa24375b6977417c69e9a6d31
Miguel Á. Sastre <sastre113@gmail.com>
2024-07-30 20:17:02 +0200

    Initial commit

---
+++ src/main/java/belazar/common/Utils.java
@@ -0,0 +1,14 @@
+package belazar.common;
+
+
+public final class Utils {
+
+    private Utils() {
+        throw new UnsupportedOperationException("");
+    }
+
+    public static void sayHello(){
+        System.out.println("Hello!");
+    }
+
+}
---
+++ src/test/java/belazar/UtilsTest.java
@@ -0,0 +1,4 @@
+package belazar;
+
+public class UtilsTest {
+}
